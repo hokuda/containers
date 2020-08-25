dc764683c2d06119c5361ae055c8d6c63352a69f
Hisanobu Okuda <hisanobu.okuda@gmail.com>
2020-08-25 18:33:54 +0900

    add xa datasource

--- mariadb-103_rhel8/configure-datasource.sh
+++ mariadb-103_rhel8/configure-datasource.sh
@@ -8,11 +8,12 @@ else
     exit
 fi
 
-../bin/jboss-cli.sh << EOF
+$JBOSS_HOME/bin/jboss-cli.sh << EOF
 embed-server --std-out=echo --server-config=standalone-ha.xml
 module add --name=org.mariadb.jdbc --resources=$FILE --dependencies=javax.api,javax.transaction.api
 /subsystem=datasources/jdbc-driver=mariadb:add(driver-module-name="org.mariadb.jdbc", driver-name="mariadb", driver-xa-datasource-class-name="org.mariadb.jdbc.MySQLDataSource")
-/subsystem=datasources/data-source=mariadb:add(jndi-name="java:jboss/datasources/mariadb", use-java-context=true,connection-url="jdbc:mysql://localhost:3306/rhsso",driver-name="mariadb",user-name="rhsso",password="password")
-/subsystem=keycloak-server/spi=connectionsJpa/provider=default:write-attribute(name=properties.dataSource, value=java:jboss/datasources/mariadb)
+/subsystem=datasources/data-source=mariadb:add(jndi-name="java:jboss/datasources/mariadb", use-java-context=true,connection-url="jdbc:mysql://localhost:3306/rhsso",driver-name="mariadb",user-name="mariadb",password="password")
+xa-data-source add --name=mariadb-xa --jndi-name=java:/jboss/datasources/mariadb-xa --driver-name=mariadb --xa-datasource-properties={"ServerName"=>"localhost","DatabaseName"=>"rhsso"} --user-name=mariadb --password=password
+/subsystem=keycloak-server/spi=connectionsJpa/provider=default:write-attribute(name=properties.dataSource, value=java:jboss/datasources/mariadb-xa)
 stop-embedded-server
 EOF
