157b180b794316b1267d6e9e155d2e9c34938f7e
Hisanobu Okuda <hisanobu.okuda@gmail.com>
2020-11-13 18:07:41 +0900

    enable LDAPS and LDAP+StartTLS

--- redhat-ds_rhel8/README.md
+++ redhat-ds_rhel8/README.md
@@ -22,4 +22,23 @@ podman exec -it redhat-ds tail -f  /var/log/dirsrv/slapd-localhost/access
 ldapsearch -v -x -H ldap://localhost:10389/ -D "cn=Directory Manager" -w password -s sub -b ou=people,dc=redhat,dc=com dn cn
 ```
 
+* ldap test with LDAP+StartTLS
+
+Disable certificate verification:
+```
+$ sudo vim /etc/openldap/ldap.conf
+TLS_REQCERT never
+```
+
+```
+$ ldapsearch -ZZ -v -x -H ldap://localhost:10389/ -D "cn=Directory Manager" -w password -s sub -b dc=redhat,dc=com "uid=demo_user" cn
+```
+
+* ldap test with LDAPS
+
+```
+$ ldapsearch -v -x -H ldaps://localhost:10636/ -D "cn=Directory Manager" -w password -s sub -b dc=redhat,dc=com "uid=demo_user" cn
+```
+
+
 The admin URL is http://localhost:19090/ to log in as root/password.
--- redhat-ds_rhel8/start-redhat-ds.sh
+++ redhat-ds_rhel8/start-redhat-ds.sh
@@ -9,3 +9,7 @@ podman build -t $IMAGE_NAME .
 podman run -d --privileged --name $CONTAINER_NAME -p $LDAP_PORT:389 -p $LDAPS_PORT:636 -p $ADMIN_PORT:9090 localhost/$IMAGE_NAME /sbin/init
 podman exec -it $CONTAINER_NAME systemctl start cockpit.socket
 podman exec -it $CONTAINER_NAME dscreate -v from-file /root/dscreate.inf
+podman exec -it $CONTAINER_NAME dsconf -D "cn=Directory Manager" -w password ldap://localost config replace nsslapd-securePort=636 nsslapd-security=on
+podman exec -it $CONTAINER_NAME dsconf -D "cn=Directory Manager" -w password ldap://localhost security certificate list
+podman exec -it $CONTAINER_NAME dsconf -D "cn=Directory Manager" -w password ldap://localhost security rsa set --tls-allow-rsa-certificates on --nss-token "internal (software)" --nss-cert-name Server-Cert
+podman exec -it $CONTAINER_NAME dsctl slapd-localhost restart
