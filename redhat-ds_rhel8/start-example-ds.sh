bed4d69ad4ac3ad1747ca1f94053e67c2e97c328
Hisanobu Okuda <hisanobu.okuda@gmail.com>
2021-10-26 08:56:02 +0900

    example

--- redhat-ds_rhel8/start-example-ds.sh
+++ redhat-ds_rhel8/start-example-ds.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-IMAGE_NAME=redhat-ds/rhel8
-CONTAINER_NAME=redhat-ds
+IMAGE_NAME=example-ds/rhel8
+CONTAINER_NAME=example-ds
 LDAP_PORT=10389
 LDAPS_PORT=10636
 ADMIN_PORT=19090
 
-podman build -t $IMAGE_NAME .
+podman build -t $IMAGE_NAME -f Dockerfile.example .
 podman run -d --privileged --name $CONTAINER_NAME -p $LDAP_PORT:389 -p $LDAPS_PORT:636 -p $ADMIN_PORT:9090 localhost/$IMAGE_NAME /sbin/init
 podman exec -it $CONTAINER_NAME systemctl start cockpit.socket
 podman exec -it $CONTAINER_NAME dscreate -v from-file /root/dscreate.inf
