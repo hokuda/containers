574d029e6151a8a27b5ef363a08c73b5cccdfa2d
hokuda <hisanobu.okuda@gmail.com>
2024-11-19 15:08:10 +0900

    aaa

--- redhat-ds_rhel8/start-example-ds.sh
+++ redhat-ds_rhel8/start-example-ds.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/sh -v
 IMAGE_NAME=example-ds/rhel8
 CONTAINER_NAME=example-ds
 LDAP_PORT=10389
@@ -6,7 +6,7 @@ LDAPS_PORT=10636
 ADMIN_PORT=19090
 
 podman build -t $IMAGE_NAME -f Dockerfile.example .
-podman run -d --privileged --name $CONTAINER_NAME -p $LDAP_PORT:389 -p $LDAPS_PORT:636 -p $ADMIN_PORT:9090 localhost/$IMAGE_NAME /sbin/init
+podman run --replace -d --privileged --name $CONTAINER_NAME -p $LDAP_PORT:389 -p $LDAPS_PORT:636 -p $ADMIN_PORT:9090 localhost/$IMAGE_NAME /sbin/init
 podman exec -it $CONTAINER_NAME systemctl start cockpit.socket
 podman exec -it $CONTAINER_NAME dscreate -v from-file /root/dscreate.inf
 podman exec -it $CONTAINER_NAME dsconf -D "cn=Directory Manager" -w password ldap://localost config replace nsslapd-securePort=636 nsslapd-security=on
